31f3dc65e21d7bd5419253f83cea69d71477e3ff
Paras Jain <pjjexpert@gmail.com>
2012-07-09 15:26:30 -0700

    Revert "Readme update"
    This reverts commit 851a84f6ebf24714531670dc9439ddfce14d9800.

--- donut/donut.js
+++ donut/donut.js
@@ -1,7 +1,8 @@
-function donut(dat) {
-  var width = 400,
-      height = 400,
-      outerRadius = Math.min(width, height) / 2,
+function donut(dat,labels,width,height) {
+  width = width || 400;
+  height = height || 400;
+  labels = labels || new Array(dat.length);
+  var outerRadius = Math.min(width, height) / 2,
       innerRadius = outerRadius * .6,
       n = dat.length,
       q = 0,
@@ -25,6 +26,7 @@ function donut(dat) {
     .append("path")
       .attr("fill", function(d, i) { return color(i); })
       .attr("d", arc);
+    //.append("");
 
   window.addEventListener("keypress", swap, false);
 
