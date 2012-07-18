34728abe200ae1330a8afcf1bb34c96256eed86b
Ajay Jain <ajayjain318@gmail.com>
2012-07-18 10:53:00 -0700

    Basic implementation of DRA wrapper

--- wrappers/dra.js
+++ wrappers/dra.js
@@ -1,7 +1,37 @@
 // Requires ../charts/line.js
 
 charts.extend({
-	DRA: function(data,sel,w,h) {
-		
+	DRA: function(obj) {
+		var xMax = (obj.deadline && typeof obj.deadline === 'number') ? obj.deadline + .5 : undefined;
+
+		obj.container = (obj.container && obj.container !== '' && obj.container !== 'undefined') ? obj.container : 'body';
+		obj.width     = (obj.width && typeof obj.width === 'number') ? obj.width : undefined;
+        obj.height    = (obj.height && typeof obj.height === 'number') ? obj.height : undefined;
+
+		this.line({
+			data: [{
+					x: 0,
+					y: 25
+				}, {
+					x: .5,
+					y: 30
+				},  {
+					x: 2,
+					y: 40
+				}, {
+					x: 2.5,
+					y: 45
+				}],
+			title: 'Reading Level',
+			xlabel: 'Time',
+			ylabel: 'DRA Score',
+			xMax: xMax,
+			yMax: 60,
+			xMarker: obj.deadline,
+			yMarker: obj.goal,
+			container: obj.container,
+			width:  obj.width,
+			height: obj.height,
+		});
 	}
 });
