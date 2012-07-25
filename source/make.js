26ec60e29dfea1ace82b9f9c3359c443ca7f0e2c
Ajay Jain <ajayjain318@gmail.com>
2012-07-25 15:50:22 -0700

    Rebuild, removed chartsBuild.js

--- source/make.js
+++ source/make.js
@@ -21,9 +21,9 @@ fs.readdir('charts', function(e, files){
 				code += fs.readFileSync('wrappers/' + file, 'ascii')+'\n';
 			}
 		});
-		fs.writeFile('../charts.js', code, function(){
-			console.log('charts.js created.');
-		});
+		// fs.writeFile('../charts.js', code, function(){
+		// 	console.log('charts.js created.');
+		// });
 		var ast = parser.parse(code);
 		ast = uglify.ast_mangle(ast);
 		ast = uglify.ast_squeeze(ast);
