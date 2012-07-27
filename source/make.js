4ba06a4b9ce260d34e56928f05677fe78a60d872
Ajay Jain <ajayjain318@gmail.com>
2012-07-26 17:53:16 -0700

    Cleanup

--- source/make.js
+++ source/make.js
@@ -1,9 +1,13 @@
-#!/usr/bin/env node
+// Copyright (C) 2012 JainLabs, Ajay Jain, Paras Jain
+// This software is icensed under a Creative Commons Attribution-NonCommercial 3.0 Unported License, found at http://creativecommons.org/licenses/by-nc/3.0/.
 
 var fs = require('fs');
 var uglifyjs = require('uglify-js');
 var parser = uglifyjs.parser;
 var uglify = uglifyjs.uglify;
+
+var comment = "// Copyright (C) 2012 JainLabs, Ajay Jain, Paras Jain\n\
+// This software is icensed under a Creative Commons Attribution-NonCommercial 3.0 Unported License, found at http://creativecommons.org/licenses/by-nc/3.0/.";
 
 var code = fs.readFileSync('./charts.js', 'ascii')+'\n';
 
@@ -21,14 +25,11 @@ fs.readdir('charts', function(e, files){
 				code += fs.readFileSync('wrappers/' + file, 'ascii')+'\n';
 			}
 		});
-		// fs.writeFile('../charts.js', code, function(){
-		// 	console.log('charts.js created.');
-		// });
 		var ast = parser.parse(code);
 		ast = uglify.ast_mangle(ast);
 		ast = uglify.ast_squeeze(ast);
 		var finalCode = uglify.gen_code(ast);
-		fs.writeFile('../charts.min.js', finalCode, function(){
+		fs.writeFile('../charts.min.js', comment+"\n"+finalCode, function(){
 			console.log('charts.min.js created.');
 		});
 	});
